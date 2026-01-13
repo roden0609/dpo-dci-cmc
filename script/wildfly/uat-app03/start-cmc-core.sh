09c486613818bf1d519d6ac39285089f7fc3708e
Roden <kcliawu@gmail.com>
2026-01-13 16:08:21 +0800

    UAT startup success

--- script/wildfly/uat-app03/start-cmc-core.sh
+++ script/wildfly/uat-app03/start-cmc-core.sh
@@ -1,6 +1,7 @@
 SERVER_BASE_DIR=/bd-ogcmr/uat/jboss/wildfly-32.0.1.Final/cmc-core
 
 rm -rf $SERVER_BASE_DIR/tmp/
+rm -rf $SERVER_BASE_DIR/configuration/standalone_xml_history/
 # TODO: interim solution, long term should not delete data completely, restart caching should handle in configuration file
 # rm -rf $SERVER_BASE_DIR/data/
 rm -rf $SERVER_BASE_DIR/data/kernel/
@@ -9,7 +10,6 @@ rm -rf $SERVER_BASE_DIR/data/tx-object-store/
 rm -rf $SERVER_BASE_DIR/data/timer-service-data/
 rm -rf $SERVER_BASE_DIR/data/activemq/
 rm -rf $SERVER_BASE_DIR/data/infinispan/
-rm -rf $SERVER_BASE_DIR/configuration/standalone_xml_history/
 
 JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=512m -Djava.net.preferIPv4Stack=true -Djava.awt.headless=true"
 export JAVA_OPTS
