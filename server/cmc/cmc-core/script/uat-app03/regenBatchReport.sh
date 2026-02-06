9aab60f2946d71bf62b4144a0f892409de8737ef
Roden <kcliawu@gmail.com>
2026-02-06 18:46:35 +0800

    added report batch script

---
+++ server/cmc/cmc-core/script/uat-app03/regenBatchReport.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. `dirname $0`/setEnv.sh
+. `dirname $0`/setEnvReport.sh
+
+export SERVLET_URL="http://${DOMAIN_NAME}:${HTTP_PORT}/dci-cmc-wss/servlet/RegenerateAdminBatchReportServlet"
+
+echo `date`: Run regenerate batch reports
+
+curl -s "${SERVLET_URL}"
+
+rc=$?
+
+echo `date`: End running regenerate batch reports
+
+exit $rc
