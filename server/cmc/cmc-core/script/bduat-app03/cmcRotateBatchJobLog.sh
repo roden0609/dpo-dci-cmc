e012f6f0daae8014ba733bb0b1e5a09b426dfb79
Roden <kcliawu@gmail.com>
2026-03-31 18:12:13 +0800

    Tested BDUAT batch job is runnable without error

--- server/cmc/cmc-core/script/bduat-app03/cmcRotateBatchJobLog.sh
+++ server/cmc/cmc-core/script/bduat-app03/cmcRotateBatchJobLog.sh
@@ -6,12 +6,11 @@ echo `date '+%F %H:%M:%S'` $0 ' Start --'
 
 . `dirname $0`/setEnv.sh
 
-ROOT_PATH=/bd-ogcmr/uat/cmc/cmc-core/script/
 FILE_DATE=`${ROOT_PATH}previous_date.sh`
 TODAY=`date +%Y%m%d`
 
-OLD_LOG_FILE=${ROOT_PATH}log/batchjob.log
-NEW_LOG_FILE=${ROOT_PATH}log/batchjob.log.${FILE_DATE}
+OLD_LOG_FILE=${ROOT_PATH}/log/batchjob.log
+NEW_LOG_FILE=${ROOT_PATH}/log/batchjob.log.${FILE_DATE}
 
 mv ${OLD_LOG_FILE} ${NEW_LOG_FILE}
 touch ${OLD_LOG_FILE}
